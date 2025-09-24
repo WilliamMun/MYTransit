5c1a970a0dd86a53046d5a6df1bcc0cabd5048c1
William <williammun03@gmail.com>
2025-09-25 01:40:47 +0800

    Display station name in main flow

--- lib.py
+++ lib.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import csv
+import pandas as pd
 
 def typewriter_effect(text, delay=0.05):
     for char in text:
@@ -15,3 +17,13 @@ def welcome_message():
     for line in text_lines:
         typewriter_effect(line)
         time.sleep(0.3)
+
+def display_station():
+    df = pd.read_csv("station_info.csv", encoding="cp1252")
+    df.columns = df.columns.str.strip()
+    df_clean = df[["station_id", "station_name", "type_of_train"]]
+    df_clean = df_clean.drop_duplicates(subset=["station_name"])
+    df_clean["formatted"] = df_clean["station_id"].astype(str) + ": " + df_clean["type_of_train"] + "- " + df_clean["station_name"]
+    station_list = df_clean["formatted"].tolist()
+    for station in station_list:
+        print(station)
--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import lib
 lib.welcome_message()
 lib.typewriter_effect("Select your starting point and destination by entering respective numbers below:")
 
+lib.display_station()
