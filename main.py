a46e75154701c2e562cd42ccdbc413fab2be078d
William <williammun03@gmail.com>
2025-09-25 16:38:14 +0800

    User input error handling

--- main.py
+++ main.py
@@ -5,28 +5,41 @@ import time
 #1: Welcome message 
 lib.welcome_message()
 
-#2: Requesting input from user
-lib.typewriter_effect("Select your starting point and destination by entering respective numbers below:")
-time.sleep(1)
-lib.display_station()
-print("\n")
-start = int(input("Enter starting station ID: "))
-end = int(input("Enter destination station ID: "))
-print("\n")
+while True:
+    #2: Requesting input from user
+    lib.typewriter_effect("Select your starting point and destination by entering respective numbers below:")
+    time.sleep(1)
+    lib.display_station()
+    print("\n")
+    start = int(input("Enter starting station ID: "))
+    end = int(input("Enter destination station ID: "))
+    print("\n")
 
-#3: Output info 1- Suggested Route 
-lib.typewriter_effect("="*60)
-print("1. Suggested Route:")
-print(lib.suggest_route(start, end))
-time.sleep(1)
-lib.typewriter_effect("="*60)
+    if 1<=start<=186 and 1<=end<=186:
+        #3: Output info 1- Suggested Route 
+        lib.typewriter_effect("="*60)
+        print("1. Suggested Route:")
+        print(lib.suggest_route(start, end))
+        time.sleep(1)
+        lib.typewriter_effect("="*60)
 
-#4: Output info 2- Fare 
+        #4: Output info 2- Fare 
 
-time.sleep(1)
-#lib.typewriter_effect("="*60)
+        time.sleep(1)
+        #lib.typewriter_effect("="*60)
 
-#5: Output info 3- Schedule 
-print("3. Schedule:")
-print(duration.get_next_train_time(start))
-lib.typewriter_effect("="*60)
+        #5: Output info 3- Schedule 
+        print("3. Schedule:")
+        print(duration.get_next_train_time(start))
+        lib.typewriter_effect("="*60)
+        time.sleep(1)
+
+        start_another=input("Would you like to start another journey? (Y/N)")
+        if start_another == "Y" or start_another == "y":
+            continue
+        else:
+            lib.typewriter_effect("Thanks for choosing MYTransit!")
+            break
+    else:
+        print("Invalid input. Please enter number between a valid range!")
+        time.sleep(1)
